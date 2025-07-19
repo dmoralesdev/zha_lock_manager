4f1e104a12475afe55c2e33f75289d5de949f86a
dmoralesdev <diegomorales17@hotmail.com>
2025-07-19 15:21:38 -0500

    fixed flow

--- custom_components/zha_lock_manager/config_flow.py
+++ custom_components/zha_lock_manager/config_flow.py
@@ -1,13 +1,1 @@
-
-from __future__ import annotations
-import voluptuous as vol
-from homeassistant import config_entries
-from .const import DOMAIN
-
-class ZhaLockManagerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
-
-    async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="ZHA Lock Manager", data={})
-        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
+# placeholder config_flow
